2126ce307518612ed710de16d44db0c4ffa7e340
g.mercurio.esterno@almaviva.it <g.mercurio.esterno@almaviva.it>
2022-06-26 09:39:43 +0200

    test updated

--- test_main.py
+++ test_main.py
@@ -6,15 +6,15 @@ client = TestClient(app)
 
 
 def test_get_root():
-    response = client.get("/")
+    response = client.get(app.url_path_for("root"))
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World!"}
 
 
 def test_post_signup_2fa_disabled():
     # Expecting Form inputs, using data parameter
-    response = client.post("/signup/",
+    response = client.post(app.url_path_for("signup"),
                            data={"username": "WalterWhite", "password": "SayMyName", "two_factor_enabled": "false"})
 
     assert response.status_code == 200
-    assert response.json() == {"username": "WalterWhite", "password": "SayMyName", "two_factor_enabled": "false"}
+    assert response.json() == {"username": "WalterWhite", "password": "SayMyName", "two_factor_enabled": False}
